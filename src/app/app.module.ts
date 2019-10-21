a02ffb76518325262e6167b2c2accb8af403cc99
Ane Decomotan <anelie.decomotan@teloworksinc.com>
2019-10-21 17:10:18 +0800

    Code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,38 +6,15 @@ import {RouterModule} from '@angular/router';
 import {AppRoutingModule} from './app.routing';
 
 import {AppComponent} from './app.component';
-import {AdminComponent} from './admin/admin.component';
 import {LandingComponent} from './landing/landing.component';
-import {ProductListComponent} from './product-list/product-list.component';
-import {NavbarComponent} from './shared/navbar/navbar.component';
-import {FooterComponent} from './shared/footer/footer.component';
 
-import {HomeModule} from './home/home.module';
-import {DbOperationsService} from './db-operations.service';
 import {HttpClientModule} from '@angular/common/http';
 import {NgxDatatableModule} from '@swimlane/ngx-datatable';
-import {PhotosComponent} from './photos/photos.component';
-import {DesignComponent} from './design/design.component';
-import {AboutComponent} from './about/about.component';
-import {RateComponent} from './rate/rate.component';
-import {MessagesComponent} from './messages/messages.component';
-import {RateListComponent} from './rate-list/rate-list.component';
-import {AuthGuard} from './auth-guard';
 
 @NgModule({
   declarations: [
       AppComponent,
-      AdminComponent,
       LandingComponent,
-      NavbarComponent,
-      FooterComponent,
-      PhotosComponent,
-      DesignComponent,
-      AboutComponent,
-      RateComponent,
-      ProductListComponent,
-      MessagesComponent,
-      RateListComponent
   ],
   imports: [
       BrowserModule,
@@ -46,10 +23,8 @@ import {AuthGuard} from './auth-guard';
       FormsModule,
       RouterModule,
       AppRoutingModule,
-      HomeModule,
       NgxDatatableModule
   ],
-  providers: [DbOperationsService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
